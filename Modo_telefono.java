a0213ba5c5769f44586ed47f1d6ff80520144e5d
TheKiesling <kie21581@uvg.edu.gt>
2021-11-13 19:29:16 -0600

    cambios

--- Modo_telefono.java
+++ Modo_telefono.java
@@ -1,5 +1,5 @@
 public interface Modo_telefono {
-    public boolean conectar();
+    public String conectar();
     public String mostrar_contactos();
     public String llamar_contacto(int index);
     public String finalizar_llamada();
--- Playlist.java
+++ Playlist.java
@@ -5,16 +5,20 @@ public class Playlist {
     ArrayList<Cancion> canciones;
     public Playlist(String nombre){
         this.nombre=nombre;
-        if(nombre.equals("Rancheras")){
-            canciones.add(new Cancion());
+        if(nombre.equals("Reggaeton")){
+            canciones.add(new Cancion("Si tu supieras", "Feid", "3:16"));
+            canciones.add(new Cancion("Fiel", "Los legendarios", "4:22"));
+            canciones.add(new Cancion("512", "Mora", "3:14"));
         }
         if(nombre.equals("Rock")){
             canciones.add(new Cancion("Hey You", "Pink Floyd","4:38"));
             canciones.add(new Cancion("One", "Metalica","7:27"));
             canciones.add(new Cancion("Losing my religion", "R.E.M.","4:28"));
         }
-        if(nombre.equals("Clasicas")){
-            canciones.add(new Cancion());
+        if(nombre.equals("DanceHall")){
+            canciones.add(new Cancion("Ley Seca", "Jhay Cortez", "4:24"));
+            canciones.add(new Cancion("2/Catorce", "Rauw Alejandro", "3:32"));
+            canciones.add(new Cancion("Dakiti", "Bad Bunny", "3:25"));
         }
     }
     public String toString(){
