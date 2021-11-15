7b2245f4eb7aaf264bb020129bddb9cedd96afc3
Montoya086 <mon21552@uvg.edu.gt>
2021-11-15 13:43:49 -0600

    comentarios

--- Radio.java
+++ Radio.java
@@ -46,14 +46,29 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
         llamada = false;
     }
     
+    /****************************************************************
+     * Encender: mensaje de radio encendida
+     * @return mensaje
+     */
     public String encender(){
         return "La radio ha sido encendida.";
     }
+    //***************************************************************
 
+    /****************************************************************
+     * Apagar: mensaje de radio apagada
+     * @return mensaje
+     */
     public String apagar(){
         return "La radio ha sido apagada.";
     }
-    
+    //***************************************************************
+
+    /****************************************************************
+     * volumen: subir y bajar volumen
+     * @return volumen
+     * @param sb
+     */
     public String volumen(boolean sb){
         if(sb){
             if(volumen<100)
@@ -64,6 +79,12 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
         }
         return "Volumen: "+volumen;
     }
+    //***************************************************************
+
+    /****************************************************************
+     * Cambiar_fm_am: cambia la frecuencia de la radio
+     * @return frecuencia
+     */
     public String cambiar_fm_am(){
         if(frecuencia.equals("FM")){
             frecuencia="AM";
@@ -72,18 +93,46 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
         }
         return "Se ha cambiado a "+frecuencia;
     }
+    //***************************************************************
+
+    /****************************************************************
+     * cambiar_emisora: cambia la frecuencia de la radio
+     * @return emisora
+     * @param emisora
+     */
     public String cambiar_emisora(int emisora){
         estacion += emisora;
         return "Se ha cambiado a la estacion "+estacion;
     }
+    //***************************************************************
+
+    /****************************************************************
+     * guardar_emisora: guardar la emisora
+     * @return estacion
+     * @param index
+     */
     public String guardar_emisora(int index){
         estaciones[index]=estacion;
         return "Estacion "+estacion+" guardada en ("+index+")";
     }
+    //***************************************************************
+
+    /****************************************************************
+     * cargar emisora: carga la emisora seleccionada
+     * @return mensaje
+     * @param index
+     */
     public String cargar_emisora(int index){
         estacion=estaciones[index];
         return "La estacion "+estacion+" ha sido cargada";
     }
+    //***************************************************************
+
+    /****************************************************************
+     * cambiar: cambiar cancion
+     * @return mensaje
+     * @param sb
+     */
     public String cambiar(boolean sb){
         if(playlist!=null){
             if(sb){
@@ -100,6 +149,12 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
             return "No se ha seleccionado una playlist";
         }
     }
+    //***************************************************************
+
+    /****************************************************************
+     * escuchar: escuchar cancion
+     * @return mensaje
+     */
     public String escuchar(){
         if(cancion !=null){
             return "Se ha iniciado la cancion "+cancion.toString();
@@ -107,16 +162,35 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
             return "No se ha seleccionado una cancion";
         }
     }
+    //***************************************************************
+
+    /****************************************************************
+     * seleccionar: seleccionar playlist
+     * @return mensaje
+     * @param index
+     */
     public String seleccionar(int index){
         playlist=playlists.get(index);
         ncancion=0;
         cancion = playlist.getCancion(ncancion);
         return "Se ha seleccionado la playlist: "+playlist.toString();
     }
+    //***************************************************************
+
+    /****************************************************************
+     * conectar: contectar telefono
+     * @return mensaje
+     */
     public String conectar(){
         telefono = true;
         return "Telefono conectado";
     }
+    //***************************************************************
+
+    /****************************************************************
+     * mostrar_contactos: mostrar contactos
+     * @return contacto
+     */
     public String mostrar_contactos(){
         if(telefono){
             String contact="";
@@ -128,6 +202,13 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
             return "No se ha conectado un telefono";
         }
     }
+    //***************************************************************
+
+    /****************************************************************
+     * llamar_contacto: llamar al contacto seleccionado
+     * @return contacto
+     * @param index
+     */
     public String llamar_contacto(int index){
         if(telefono){
             llamada = true;
@@ -137,6 +218,12 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
             return "No se ha conectado un telefono";
         }
     }
+    //***************************************************************
+
+    /****************************************************************
+     * finalizar_llamada: finaliza llamada
+     * @return mensaje
+     */
     public String finalizar_llamada(){
         if(llamada){
             llamada=false;
@@ -145,6 +232,12 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
             return "No hay una llamada activa";
         }
     }
+    //***************************************************************
+
+    /****************************************************************
+     * getPlaylist: regresa las playlist
+     * @return playlists
+     */
     public String getPlaylists(){
         String str_playlist="";
         for(int i=0;i<playlists.size();i++){
@@ -152,6 +245,8 @@ public abstract class Radio implements Radio_basica, Modo_radio, Modo_reproducci
         }   
         return str_playlist;
     }
+    //***************************************************************
+    
     public abstract String modo_telefono();
 
     public abstract String modo_productividad(String cosa);
