5ee6f291cdda94dd5bcef605170aca3a3c6fad59
Deloz09 <84214640+Deloz09@users.noreply.github.com>
2021-11-15 12:33:39 -0600

    Update 4 - Corrección Productividad

--- Radio_C.java
+++ Radio_C.java
@@ -18,15 +18,11 @@ public class Radio_C extends Radio{
     }
 
     public String modo_productividad(String cosa){
-        return "Se ha activado el modo productividad." + cosa;
+        String msg = "El pronostico del dia de hoy "+"["+cosa+"]"+" es: \nSoleado";
+        return msg;
     }
 
     public String Cambiar_llamada_espera(){
         return "Se ha colocado la llamada en espera.";
-    }
-
-    public String pronostico_tiempo(String dia){
-        String msg = "El pronostico del dia de hoy "+"["+dia+"]"+" es: \nSoleado";
-        return msg;
     }
 }
--- Radio_S.java
+++ Radio_S.java
@@ -18,15 +18,12 @@ public class Radio_S extends Radio{
     }
 
     public String modo_productividad(String cosa){
-        return "Se ha activado el modo productividad." + cosa;
+        String msg = "Se ha planificado el viaje hacia "+cosa+".";
+        return msg;
     }
 
     public String Cambiar_bocinas_auriculares(){
         return "Se ha cambiado de bocinas a auriculares.";
     }
 
-    public String planificar_viaje(String destino){
-        String msg = "Se ha planificado el viaje hacia "+destino+".";
-        return msg;
-    }
 }
